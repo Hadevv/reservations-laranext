0bf92228146b151c748e62c41ae6f797c45491cc
Hadevv <ademeure29@gmail.com>
2024-02-26 22:59:25 +0100

    chore: 🐶 Add pre-commit script for separating backend and frontend changes

--- .husky/pre-commit.sh
+++ .husky/pre-commit.sh
@@ -1,19 +1,16 @@
-# Récupère la liste des fichiers modifiés
+
+# .husky/pre-commit.sh
+
 changed_files=$(git diff --cached --name-only)
 
-# Vérifie s'il y a des fichiers modifiés dans le backend
-backend_changes=$(echo "$changed_files" | grep "^backend/")
+backend_changes=$(echo "$changed_files" | grep "^packages/laravel-api/")
+frontend_changes=$(echo "$changed_files" | grep "^packages/nextjs-frontend/")
 
-# Vérifie s'il y a des fichiers modifiés dans le frontend
-frontend_changes=$(echo "$changed_files" | grep "^frontend/")
-
-# Empêche les commits contenant à la fois du backend et du frontend
 if [ -n "$backend_changes" ] && [ -n "$frontend_changes" ]; then
   echo "Erreur : Ne committez pas du backend et du frontend en même temps."
   exit 1
 fi
 
-# Ajoute automatiquement [backend] ou [frontend] au message de commit
 if [ -n "$backend_changes" ]; then
   sed -i.bak '1s/^/[backend] /' "$1"
 fi
@@ -23,3 +20,4 @@ if [ -n "$frontend_changes" ]; then
 fi
 
 exit 0
+
