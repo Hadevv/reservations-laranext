1ff5355745e388d44853125f2d393cfcb247f8ca
jerome <jeromedelodder.dev@gmail.com>
2024-03-03 12:36:18 +0100

    ajout du controller permetant de supprimer

--- packages/laravel-api/app/Models/User.php
+++ packages/laravel-api/app/Models/User.php
@@ -11,6 +11,8 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    public mixed $id;
+
     /**
      * The attributes that are mass assignable.
      *
--- packages/laravel-api/routes/auth.php
+++ packages/laravel-api/routes/auth.php
@@ -35,3 +35,6 @@ Route::post('/email/verification-notification', [EmailVerificationNotificationCo
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
                 ->middleware('auth')
                 ->name('logout');
+
+Route::post('/user/{user}/delete', [DeleteUserController::class, 'destroy'])
+    ->name('user.destroy');
